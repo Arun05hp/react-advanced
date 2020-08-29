0d82b6eba745a551cdc76577fbada54c8a93e959
Arun Kumar <kumar05.962013@gmail.com>
2020-08-29 11:08:03 +0530

    useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Movie from "./hoc/Movie";
 import "./App.css";
+import Counter from "./hooks/Counter";
 
 function App() {
-  return <Movie />;
+  return (
+    <div>
+      <Movie />
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/hooks/Counter.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+function Counter(props) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = ` Counter: ${count}`;
+  }, [count]);
+
+  return (
+    <div>
+      Counter: {count}
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
+}
+
+export default Counter;
