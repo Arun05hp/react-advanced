7a98cea8b82f4b717d3a63c3022c2096481e3f42
Arun Kumar <kumar05.962013@gmail.com>
2020-08-29 21:51:01 +0530

    use Context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import Movie from "./hoc/Movie";
 import "./App.css";
 import Counter from "./hooks/Counter";
 import Users from "./hooks/Users";
+import MoviePage from "./context/MoviePage";
+import UserContext from "./context/userContext";
 
-function App() {
-  return (
-    <div>
-      <Movie />
-      <Counter />
-      <Users />
-    </div>
-  );
+class App extends Component {
+  state = { currentUser: { name: "Ak" } };
+  render() {
+    return (
+      <UserContext.Provider value={this.state.currentUser}>
+        <div>
+          <Movie />
+          <Counter />
+          <Users />
+          <MoviePage />
+        </div>
+      </UserContext.Provider>
+    );
+  }
 }
 
 export default App;
