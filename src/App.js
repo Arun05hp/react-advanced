c0a8bafaf30ff0119fc300158d230a55ef9efe26
Arun Kumar <kumar05.962013@gmail.com>
2020-08-29 22:06:43 +0530

    updating context

--- src/App.js
+++ src/App.js
@@ -5,17 +5,30 @@ import Counter from "./hooks/Counter";
 import Users from "./hooks/Users";
 import MoviePage from "./context/MoviePage";
 import UserContext from "./context/userContext";
+import Login from "./context/Login";
 
 class App extends Component {
-  state = { currentUser: { name: "Ak" } };
+  handleLoggedIn = (username) => {
+    console.log("getting the user: " + username);
+    const user = { name: "Ak" };
+    this.setState({ currentUser: user });
+  };
+
+  state = { currentUser: null };
   render() {
     return (
-      <UserContext.Provider value={this.state.currentUser}>
+      <UserContext.Provider
+        value={{
+          currentUser: this.state.currentUser,
+          onLoggedIn: this.handleLoggedIn,
+        }}
+      >
         <div>
           <Movie />
           <Counter />
           <Users />
           <MoviePage />
+          <Login />
         </div>
       </UserContext.Provider>
     );
